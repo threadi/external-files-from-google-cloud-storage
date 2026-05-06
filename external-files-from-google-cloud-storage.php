d08e753bd2d8430a5cc2e8d9d91dd5769c8930be
Thomas Zwirner <tzwirne@gmx.de>
2026-05-06 19:55:32 +0200

    updated typos

--- external-files-from-google-cloud-storage.php
+++ external-files-from-google-cloud-storage.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       External files from Google Cloud Storage in Media Library
- * Description:       Extends External files in Media Library for usage of Google Cloud Storage.
+ * Description:       Extends External files in Media Library for use with Google Cloud Storage.
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Requires Plugins:  external-files-in-media-library
